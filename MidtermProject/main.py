f56fcfc7346794ecbe09d723025b91062c68d62d
Noble Kalish <NobleKalish>
2021-03-03 15:28:49 -0500

    init

---
+++ MidtermProject/main.py
@@ -0,0 +1,20 @@
+
+
+def main():
+    photo1, photo2 = None, None
+    feature_detection(photo1, photo2)
+    generate_summary()
+    pass
+
+
+def feature_detection(photo1, photo2):
+    pass
+
+
+def generate_summary():
+    pass
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
