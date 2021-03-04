d85e67e35e7a5a6dac2abd4b3dd61ff82d3b0f9f
T-Reiser <treiser000@gmail.copm>
2021-03-03 23:54:11 -0500

    Added file name manipulator and shell

--- MidtermProject/main.py
+++ MidtermProject/main.py
@@ -1,10 +1,36 @@
-
+import os
+import shutil
 
 def main():
-    photo1, photo2 = None, None
-    feature_detection(photo1, photo2)
+
+
+
+
+    dir_path = os.path.dirname(os.path.realpath(__file__)) + "\DNIM\Image"
+
+    groupnum = 0
+    finalrestingplace = os.path.dirname(os.path.realpath(__file__)) + "\\final\\"
+    finalrestingplace = os.path.normpath(finalrestingplace)
+
+    createcopy = False  # CHANGE THIS TO FALSE TO PREVENT COPYING OVER!!!!!!
+    if(createcopy):
+        for root, subdirs, files in os.walk(dir_path):
+
+            print("scanning root directory" + root)
+            picnum = 1
+            for f in files:
+                print(f)
+                if f.endswith(".jpg"):
+                    shutil.copyfile(root+'\\'+f,finalrestingplace + "\group"+str(groupnum)+"_pic"+str(picnum)+".jpg")
+                    picnum+=1
+            groupnum+=1
+
+    for entry in os.scandir(finalrestingplace): #iterate over every file in final
+        photo1 = entry # entry is dict with key filepath and value filename
+        for comparator in os.scandir(finalrestingplace): #compare every other image in the set
+            if comparator[0] != entry[0]:
+                feature_detection(entry, comparator)
     generate_summary()
-    pass
 
 
 def feature_detection(photo1, photo2):
