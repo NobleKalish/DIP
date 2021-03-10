e0053bd8a51b842a4b136ebd5d119f8f3031c513
Noble Kalish <NobleKalish>
2021-03-09 19:52:08 -0500

    added flann

--- MidtermProject/main.py
+++ MidtermProject/main.py
@@ -9,7 +9,7 @@ from threading import Thread
 def main():
     successes = 0
     total = 0
-    
+
     dir_path = os.path.dirname(os.path.realpath(__file__)) + "\DNIM\Image"
 
     groupnum = 0
@@ -30,9 +30,8 @@ def main():
                     picnum += 1
             groupnum += 1
 
-
     threads = []
-    for n in range(1,10): # Noble (10,14) Tyler (14,18)
+    for n in range(10, 11):  # Noble (10,14) Tyler (14,18)
         print("Spawning thread " + str(n))
         dir = finalrestingplace + "/group" + str(n)
         dir = os.path.normpath(dir)
@@ -44,13 +43,10 @@ def main():
         t.join()
 
 
-
-
-
 def thread_func(finalrestingplace):
     total = 0
     successes = 0
-    
+
     for entry in os.scandir(finalrestingplace):  # iterate over every file in final
         total += 1
         sift = cv.SIFT_create()
@@ -61,15 +57,15 @@ def thread_func(finalrestingplace):
         time = datetime.now()
         print(time.strftime("%H:%M:%S"))
         entry_dict = defaultdict(
-        list)  # key string {representing group} value list of ints {contains all matches to images in said group}
+            list)  # key string {representing group} value list of ints {contains all matches to images in said group}
         for group in os.scandir(os.path.dirname(finalrestingplace)):
             for comparator in os.scandir(group):  # compare every other image in the set
                 currgroup = group.name
                 if comparator.path != entry.path:
-                    entry_dict[currgroup].append(feature_detection(sift, description1, comparator.path))
+                    entry_dict[currgroup].append(flann(sift, description1, comparator.path))
         if generate_summary(entry_dict, os.path.basename(finalrestingplace)):
             successes += 1
-    
+
     print("Report for " + os.path.basename(finalrestingplace))
     print("Total images tested: " + str(total))
     print("Images matched correctly: " + str(successes))
@@ -77,12 +73,8 @@ def thread_func(finalrestingplace):
 
 
 def feature_detection(sift, description1, photo2):
-    #img1 = cv.imread(photo1, cv.IMREAD_GRAYSCALE)
     img2 = cv.imread(photo2, cv.IMREAD_GRAYSCALE)
-    # Initiate SIFT detector
-    # sift = cv.SIFT_create()
     # find the keypoints and descriptors with SIFT
-    #keypoints1, description1 = sift.detectAndCompute(img1, None)
     keypoints2, description2 = sift.detectAndCompute(img2, None)
     # BFMatcher with default params
     bf = cv.BFMatcher()
@@ -93,6 +85,26 @@ def feature_detection(sift, description1, photo2):
         if m.distance < 0.75 * n.distance:
             good.append([m])
     return len(good)  # return number of matches between these two images
+
+
+def flann(sift, description1, photo2):
+    img2 = cv.imread(photo2, cv.IMREAD_GRAYSCALE)  # trainImage
+    # Initiate SIFT detector
+    # find the keypoints and descriptors with SIFT
+    kp2, des2 = sift.detectAndCompute(img2, None)
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)  # or pass empty dictionary
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(description1, des2, k=2)
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0, 0] for i in range(len(matches))]
+    # ratio test as per Lowe's paper
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.7 * n.distance:
+            matchesMask[i] = [1, 0]
+    return len(matchesMask)
 
 
 def generate_summary(dict, group):
